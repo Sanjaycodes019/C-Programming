2afa6ddbc2ca5624eb82f014a477004a145c3df4
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-03-08 12:26:08 +0000

    commit

---
+++ 01.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+    /* 
+    int -> whole numbers (4 bytes)  ( 1 bytes = 8 bits )
+    float -> decimal numbers (4 bytes)
+    double -> more precise decimal (8 bytes)
+    char -> single character (1 bytes)
+    long -> big integers (8 bytes)
+
+    variable: a box to store data
+    declaration: data_type data_name = data_value;
+    example: int a = 10;
+    char a = 's';
+
+    naming rule of variable: no space, no number at start, so speical character
+
+    int x = 10;
+    x = 20;
+
+    const : a value that never changes
+
+
+    taking input:
+
+    scanf("format_specifier", &variable);
+
+    &(ampersand) -> gives the memory address of the variable
+
+    
+    int age;
+    printf("Enter your age: "); // output
+    scanf("%d", &age); // taking input
+
+    printf("your age is: %d\n", age);
+
+
+-> different types of input
+
+int(integer) -> %d
+float(decimal number) -> %f
+char(single character) -> %c
+double(large decimal number) -> %lf
+string(multiple character) -> %s
+
+
+*/
+int main(){
+
+    int roll_no;
+    float marks;
+    char grade;
+
+    
+
+    return 0;
+}
